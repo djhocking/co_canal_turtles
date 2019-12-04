6f4e53124302f9ec2edfd3e283b417ed272bd30b
Daniel Hocking <djhocking@gmail.com>
2019-12-04 15:34:48 -0500

    adding nimble and new prep for zeros trick

--- Code/run_nimble.R
+++ Code/run_nimble.R
@@ -7,7 +7,20 @@ library(nimble)
 ######### Load Data from Previous script #########
 
 testing <- TRUE
-run_date <- "2019-11-30"
+run_date <- "2019-12-03"
+
+if(testing) {
+  ni = 101
+  nt = 1
+  nc = 2
+  nb = 1
+} else {
+  na = 5000
+  nb = 5000
+  ni = 25000
+  nc = min(6, detectCores())
+  nt = 3
+}
 
 ######### Load Data from Previous script #########
 
@@ -50,17 +63,25 @@ scr_reg <- nimbleCode( {
   alpha2 ~ dnorm(0, 1 / 3 / 3) # Trap behavior universal distribution across sites
   mu_0 ~ dnorm(0, 1 / 2 / 2)
   sd_0 ~ dunif(0, 10)
+  mu_1 ~ dnorm(0, 1 / 2 / 2)
+  sd_1 ~ dunif(0, 10)
   alpha_1_sex ~ dnorm(0, 1 / 3 / 3)
+  beta_0 ~ dnorm(0, pow(3, -2))
+  beta_1 ~ dnorm(0, pow(3, -2))
+  beta_2 ~ dnorm(0, pow(3, -2))
   
   for(g in 1:n_sites) {
     psi[g] ~ dunif(0, 1) # prob of individual being in the population
     psi.sex[g] ~ dunif(0, 1) 
-    alpha0[g] ~ dnorm(mu_0, sd_0)
-    mu_1[g] ~ dnorm(0, 1 / 3 / 3)
+    alpha_1_int[g] ~ dnorm(mu_1, pow(sd_1, -2))
     
     for(t in 1:2) {
       sigma[g, t] <- pow(1 / (2*alpha1[g, t]), 0.5) # sd of half normal
-      log(alpha1[g, t]) <- mu_1[g] + alpha_1_sex * (t-1) # affect of being female on home range
+      log(alpha1[g, t]) <- alpha_1_int[g] + alpha_1_sex * (t-1) # affect of being female on home range
+    }
+    
+    for(k in 1:K) {
+      alpha0[g, k] ~ dnorm(mu_0, sd_0)
     }
     
     for(i in 1:M) {
@@ -73,7 +94,7 @@ scr_reg <- nimbleCode( {
         d[g,i,j] <- abs(s[g, i] - trap_locs[g, j])
         
         for(k in 1:K) {
-          logit(p0[g, i, j, k]) <- alpha0[g] + (alpha2 * C[i, k, g])
+          logit(p0[g, i, j, k]) <- alpha0[g, k] + (alpha2 * C[i, k, g])
           y[i, j, k, g] ~ dbern(p[g, i, j, k])
           p[g, i, j, k] <- z[g, i] * p0[g, i, j, k] * exp(- alpha1[g, Sex2[g, i]] * d[g, i, j] * d[g, i, j])
         } # i
@@ -84,6 +105,7 @@ scr_reg <- nimbleCode( {
     N[g] <- sum(z[g , ])
     density[g] <- sum(z[g , ]) / (xlim[g, 2] - xlim[g, 1]) # divided distances by 100 so calculates turtles per 100 m of canal
     
+    site_zeros[g] ~ dnorm(density[g] - (beta_0 + beta_1 * forest[g] + beta_2 * depth[g]), pow(3, -2))
   } # g
   
 })
@@ -95,7 +117,12 @@ scr_reg <- nimbleCode( {
 forest_std <- rnorm(12, 0, 2)
 depth_std <- rnorm(12, 0, 2)
 #############################
+# Real covariate data
+forest <- read.csv(file = "Data/LandUse/Forest_Cover_SingleColumn.csv", header = FALSE)
+depth <- read.csv(file = "Data/LandUse/Avg_Depth_m.csv", header = TRUE)
 
+forest_std <- as.numeric(scale(forest))
+depth_std <- as.numeric(scale(depth))
 
 jags_data_site <- list(y = EM_array, 
                        Sex = Sex, 
@@ -106,6 +133,7 @@ jags_data_site <- list(y = EM_array,
                        max_trap = max_trap, 
                        forest = forest_std,
                        depth = depth_std,
+                       site_zeros = rep(0, 12),
                        C = C, 
                        n_sites = G) #, n_ind = n_ind)
 # "initial values for the observed data have to be specified as NA"
@@ -116,20 +144,31 @@ initsf <- function() {
        psi.sex = runif(n_sites))
 }
 
-parameters <- c("sigma", "density", "N", "alpha2", "alpha0", "alpha1", "mu_0", "sd_0", "mu_1", "alpha_1_sex") #
+parameters <- c("sigma", "density", "N", "alpha2", "alpha0", "alpha1", "mu_0", "sd_0", "mu_1", "sd_1", "alpha_1_sex", "beta_0", "beta_1", "beta_2") #
 
 samples <- nimbleMCMC(
   code = scr_reg,
   constants = jags_data_site, ## provide the combined data & constants as constants
   inits = initsf,
   monitors = parameters,
-  niter = 100,
+  niter = 20,
   nburnin = 1,
   thin = 1,
   nchains = 1)
 
 
 plot(samples[ , "alpha2"], type = "l", xlab = "iteration",
+     ylab = expression(alpha))
+
+plot(samples[ , "density[1]"], type = "l", xlab = "iteration",
+     ylab = expression(alpha))
+
+plot(samples[ , "beta_1"], type = "l", xlab = "iteration",
+     ylab = expression(alpha))
+plot(samples[ , "beta_2"], type = "l", xlab = "iteration",
+     ylab = expression(alpha))
+
+plot(samples[ , "density[1]"], type = "l", xlab = "iteration",
      ylab = expression(alpha))
 
 if(run_par) {
@@ -162,6 +201,10 @@ if(run_par) {
   stopCluster(cl)
 }
 
+if(!dir.exists("Results/nimble")) dir.create("Results/nimble", recursive = TRUE)
+saveRDS(out, "Results/nimble/all_site_test_", run_date, ".rds")
+saveRDS(out, paste0("Results/nimble/all_site_final_", run_date, ".rds"))
+
 library(bayesplot)
 post_mat <- as.matrix(out)
 
@@ -171,6 +214,12 @@ p <- mcmc_trace(post_mat,  regex_pars = c("N"), n_warmup = 10) #, facet_args = l
 p + facet_text(size = 15)
 p <- mcmc_trace(post_mat,  regex_pars = c("sigma"), n_warmup = 10) #, facet_args = list(nrow = 2, labeller = label_parsed))
 p + facet_text(size = 15)
+p <- mcmc_trace(post_mat,  regex_pars = c("alpha_1_sex"), n_warmup = 10) #, facet_args = list(nrow = 2, labeller = label_parsed))
+p + facet_text(size = 15)
+
+p <- mcmc_trace(post_mat,  regex_pars = c("alpha2"), n_warmup = 10) #, facet_args = list(nrow = 2, labeller = label_parsed))
+p + facet_text(size = 15)
+
 
 
 
