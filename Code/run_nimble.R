1cbbad69553cfa09f9fdce53af902f4bc6764794
Daniel Hocking <djhocking@gmail.com>
2019-12-10 17:00:13 -0500

    getting read to add zeros trick

--- Code/run_nimble.R
+++ Code/run_nimble.R
@@ -7,7 +7,7 @@ library(nimble)
 ######### Load Data from Previous script #########
 
 testing <- TRUE
-run_date <- "2019-12-03"
+run_date <- "2019-12-10"
 
 if(testing) {
   ni = 101
@@ -53,10 +53,63 @@ df_M
 
 # restrict M to being the maximum size of the data array so the jags loops doesn't go out of bounds
 M <- if_else(df_M$M > M, M, trunc(df_M$M))
-M <- if_else(df_M$M < max_ind_sp, max_ind_sp, M) # real captures can be up to row 750 at any site, all after are definitely augmentation
+# M <- if_else(df_M$M < max_ind_sp, max_ind_sp, M) # real captures can be up to row 750 at any site, all after are definitely augmentation - not anymore
 
 
 ######### NIMBLE Model ##########
+
+
+scr_zeros <- nimbleCode( {
+  
+  alpha2 ~ dnorm(0, 1 / 3 / 3) # Trap behavior universal distribution across sites
+  mu_0 ~ dnorm(0, 1 / 2 / 2)
+  sd_0 ~ dunif(0, 10)
+  mu_1 ~ dnorm(0, 1 / 2 / 2)
+  sd_1 ~ dunif(0, 10)
+  alpha_1_sex ~ dnorm(0, 1 / 3 / 3)
+  beta_0 ~ dnorm(0, pow(3, -2))
+  beta_1 ~ dnorm(0, pow(3, -2))
+  beta_2 ~ dnorm(0, pow(3, -2))
+  
+  for(g in 1:n_sites) {
+    psi[g] ~ dunif(0, 1) # prob of individual being in the population
+    psi.sex[g] ~ dunif(0, 1) 
+    alpha_1_int[g] ~ dnorm(mu_1, pow(sd_1, -2))
+    
+    for(t in 1:2) {
+      sigma[g, t] <- pow(1 / (2*alpha1[g, t]), 0.5) # sd of half normal
+      log(alpha1[g, t]) <- alpha_1_int[g] + alpha_1_sex * (t-1) # affect of being female on home range
+    }
+    
+    for(k in 1:K) {
+      alpha0[g, k] ~ dnorm(mu_0, sd_0)
+    }
+    
+    for(i in 1:M) {
+      Sex[g, i] ~ dbern(psi.sex[g])
+      Sex2[g, i] <- Sex[g, i] + 1
+      z[g, i] ~ dbern(psi[g])
+      s[g, i] ~ dunif(xlim[g, 1], xlim[g, 2]) ##??
+      
+      for(j in 1:max_trap[g]) { 
+        d[g,i,j] <- abs(s[g, i] - trap_locs[g, j])
+        
+        for(k in 1:K) {
+          logit(p0[g, i, j, k]) <- alpha0[g, k] + (alpha2 * C[i, k, g])
+          y[i, j, k, g] ~ dbern(p[g, i, j, k])
+          p[g, i, j, k] <- z[g, i] * p0[g, i, j, k] * exp(- alpha1[g, Sex2[g, i]] * d[g, i, j] * d[g, i, j])
+        } # i
+      } # j
+    } # k
+    
+    # Derived parameters
+    N[g] <- sum(z[g , ])
+    density[g] <- sum(z[g , ]) / (xlim[g, 2] - xlim[g, 1]) # divided distances by 100 so calculates turtles per 100 m of canal
+    
+    site_zeros[g] ~ dnorm(density[g] - (beta_0 + beta_1 * forest[g] + beta_2 * depth[g]), pow(3, -2))
+  } # g
+  
+})
 
 scr_reg <- nimbleCode( {
   
@@ -119,23 +172,23 @@ forest_std <- as.numeric(scale(forest))
 depth_std <- as.numeric(scale(depth))
 
 jags_data_site <- list(y = EM_array, 
-                       Sex = Sex, 
+                       Sex = sex, 
                        trap_locs = trap_locs, 
-                       K=K, 
+                       K=n_days, 
                        M=M, 
                        xlim=xlim, 
-                       max_trap = max_trap, 
+                       max_trap = n_traps_site$max_trap, 
                        forest = forest_std,
                        depth = depth_std,
                        site_zeros = rep(0, 12),
-                       C = C, 
-                       n_sites = G) #, n_ind = n_ind)
+                       C = recaptured, 
+                       n_sites = n_sites) #, n_ind = n_ind)
 # "initial values for the observed data have to be specified as NA"
 initsf <- function() {
-  list(s = t(sst), 
-       z = Zst, 
-       psi = runif(n_sites), 
-       psi.sex = runif(n_sites))
+  list(s = s_st, 
+       z = Z_st, 
+       psi = runif(n_sites, psi_st*0.5, psi_st*2), 
+       psi.sex = runif(n_sites, 0.3, 0.8))
 }
 
 parameters <- c("sigma", "density", "N", "alpha2", "alpha0", "alpha1", "mu_0", "sd_0", "mu_1", "sd_1", "alpha_1_sex", "beta_0", "beta_1", "beta_2") #
